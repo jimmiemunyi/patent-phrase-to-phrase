c95e60aef0ca7a7d86ddace2d13a99e1d1e8f62d
Jimmie Munyi <jimmiemunyi@gmail.com>
2023-06-13 06:03:17 +0300

    started using onecycle scheduler

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from functools import partial
 
 import pandas as pd
+import numpy as np
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
@@ -15,14 +16,15 @@ from torch.utils.data import DataLoader
 
 from fastcore.xtras import Path  # for ls
 
-from datasets import Dataset
+import datasets
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers.data.data_collator import default_data_collator
 
 from torch.optim import AdamW
-from torch.optim.lr_scheduler import LinearLR
+from torch.optim.lr_scheduler import LinearLR, OneCycleLR
 from torchmetrics import PearsonCorrCoef
 from composer.models.huggingface import HuggingFaceModel
+from composer.loggers import WandBLogger
 from composer import Trainer
 
 
@@ -35,14 +37,28 @@ logger = colorlog.getLogger("TRAIN")
 logger.setLevel(colorlog.INFO)
 
 
-def process_df(df: pd.DataFrame, train: bool = True):
-    df["input"] = (
-        "TEXT1: " + df.context + "; TEXT2: " + df.target + "; ANC1: " + df.anchor
-    )
-    dataset = Dataset.from_pandas(df)
+def process_df(df: pd.DataFrame, sep_token, train: bool = True):
+    df["section"] = df.context.str[0]
+    df["input"] = df.context + sep_token + df.anchor + sep_token + df.target
+    dataset = datasets.Dataset.from_pandas(df)
     if train:
         dataset = dataset.rename_columns({"score": "labels"})
     return dataset
+
+
+def create_val_split(df: pd.DataFrame, val_prop: float = 0.2, seed: int = 42):
+    anchors = df.anchor.unique()
+    np.random.seed(seed)
+    np.random.shuffle(anchors)
+    print(f"Sample Anchors: {anchors[:5]}")
+    val_sz = int(len(anchors) * val_prop)
+    val_anchors = anchors[:val_sz]
+    is_val = np.isin(df.anchor, val_anchors)
+    idxs = np.arange(len(df))
+    val_idxs = idxs[is_val]
+    trn_idxs = idxs[~is_val]
+
+    return trn_idxs, val_idxs
 
 
 def tokenize_func(batch, tokenizer):
@@ -53,6 +69,15 @@ def tokenize_func(batch, tokenizer):
         max_length=256,
         return_tensors="pt",
     )
+
+
+def predict(trainer, test_dl):
+    preds = trainer.predict(test_dl)[0]["logits"].numpy().astype(float)
+    preds = np.clip(preds, 0, 1)
+    preds = preds.round(2)
+    preds = preds.squeeze()
+
+    return preds
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
@@ -66,31 +91,35 @@ def train(cfg: DictConfig):
     train_df = pd.read_csv(path / "train.csv")
     test_df = pd.read_csv(path / "test.csv")
 
+    # loading the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(cfg.train.checkpoint)
+    # sep_token = tokenizer.sep_token
+    sep_token = " [s] "
+
     # processing the dataset
     logger.info("Processing the dataset")
-    train_ds = process_df(train_df)
-    eval_ds = process_df(test_df, train=False)
-    print(train_ds)
+    train_ds = process_df(train_df, sep_token)
+    eval_ds = process_df(test_df, sep_token, train=False)
+    print(train_ds[0])
 
     # tokenizing the dataset
     logger.info("Tokenizing the dataset")
-    tokenizer = AutoTokenizer.from_pretrained(cfg.train.checkpoint)
-
     tokenize = partial(tokenize_func, tokenizer=tokenizer)
     train_tok_ds = train_ds.map(tokenize, batched=True, batch_size=None)
     eval_tok_ds = eval_ds.map(tokenize, batched=True, batch_size=None)
     print(train_tok_ds)
     print(
-        f"""Sample Input :{train_tok_ds[0]["input"]}
+        f"""Sample Input :{train_tok_ds[0]["input"]}    
         \nSample Input Ids: {train_tok_ds[0]["input_ids"]}
         \nSample Attention Mask: {train_tok_ds[0]["attention_mask"]}
         \nSample Label: {train_tok_ds[0]["labels"]}"""
     )
 
     # splitting the dataset
+    trn_idxs, val_idxs = create_val_split(train_df, val_prop=cfg.train.val_size)
     logger.info("Splitting the dataset")
-    train_dds = train_tok_ds.train_test_split(
-        test_size=cfg.train.val_size, shuffle=True, seed=42
+    train_dds = datasets.DatasetDict(
+        {"train": train_tok_ds.select(trn_idxs), "test": train_tok_ds.select(val_idxs)}
     )
     print(train_dds)
     print("Checking if the dataset lengths are similar")
@@ -111,13 +140,12 @@ def train(cfg: DictConfig):
         shuffle=False,
         collate_fn=default_data_collator,
     )
-    # get a sample batch and print the first element
-    print("Sample batch")
-    batch = next(iter(val_dl))
-    print(batch["input_ids"][0])
-    print(batch["token_type_ids"][0])
-    print(batch["attention_mask"][0])
-    print(batch["labels"][0])
+    test_dl = DataLoader(
+        eval_tok_ds,
+        batch_size=64,
+        shuffle=False,
+        collate_fn=default_data_collator,
+    )
 
     # loading the model
     logger.info("Loading the model and training configurations")
@@ -144,7 +172,24 @@ def train(cfg: DictConfig):
         optimizer, start_factor=1.0, end_factor=0, total_iters=150
     )
 
+    # extracting from 4ep
+    epochs = int(cfg.train.max_duration.split("ep")[0])
+
+    one_cycle_lr = OneCycleLR(
+        optimizer,
+        max_lr=cfg.train.lr,
+        steps_per_epoch=len(train_dl),
+        epochs=epochs,
+    )
+
     # device = "cuda" if torch.cuda.is_available() else "cpu"
+    wandb_logger = WandBLogger(
+        project="patent-phrase-to-phrase",
+    )
+    if cfg.train.run_name:
+        run_name = cfg.train.run_name
+    else:
+        run_name = cfg.train.checkpoint.split("/")[-1]
 
     trainer = Trainer(
         model=composer_model,
@@ -152,16 +197,24 @@ def train(cfg: DictConfig):
         eval_dataloader=val_dl,
         max_duration=cfg.train.max_duration,
         optimizers=optimizer,
-        schedulers=[linear_lr_decay],
+        schedulers=[one_cycle_lr],
         device="gpu",
         precision="amp_fp16",
-        # seed=17,
+        loggers=[wandb_logger],
+        run_name=run_name,
+        step_schedulers_every_batch=True,
     )
 
     # training the model
     logger.info("Training the model")
     trainer.fit()
-    print(trainer.state.eval_metric_values)
+
+    # evaluating the model
+    if cfg.create_csv:
+        logger.info("Evaluating the model")
+        preds = predict(trainer, test_dl)
+        submission = datasets.Dataset.from_dict({"id": eval_ds["id"], "score": preds})
+        submission.to_csv("submission.csv", index=False)
 
 
 if __name__ == "__main__":
